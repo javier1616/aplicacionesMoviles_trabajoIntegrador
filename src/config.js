acd9dbcb32acae3c4256182e3446e44f55039a58
javier1616 <javiersolis.unaj@gmail.com>
2026-04-15 09:43:25 -0300

    se agrega wiki API a config.js

--- src/config.js
+++ src/config.js
@@ -15,14 +15,24 @@ export const jolpica_url_circuits = jolpica_url + "2026/circuits"
 // OpenF1API - https://openf1.org/docs/#api-endpoints
 export const openf1API_url = "https://api.openf1.org/v1/"
 
-//examples:
+//example: --> https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315
 
-//https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315
+
+//Como jopilca trae la pagina wiki de los circuitos/pilotos, podes usar la API publica de wikipedia
+//para traer la imagen del circuito: 
+// 1. Haces el request a jopilca
+// 2. Extraes de la url que te trae el nombre del circuito
+// 3. Lo colocas al final de esta URL
+// 4. Te devuelve un JSON, con una url en la propiedad source. Esa es la url de la imagen del circuito
+export const wiki_API = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles="
+
+//EXAMPLE   --> https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=Circuit_of_the_Americas
+
 
 
 /*
 
-   "season": "https://api.jolpi.ca/ergast/f1/seasons",
+    "season": "https://api.jolpi.ca/ergast/f1/seasons",
     "circuit": "https://api.jolpi.ca/ergast/f1/circuits",
     "race": "https://api.jolpi.ca/ergast/f1/2026/races",
     "constructor": "https://api.jolpi.ca/ergast/f1/2026/constructors",
